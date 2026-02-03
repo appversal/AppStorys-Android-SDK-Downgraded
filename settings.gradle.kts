20624f6200d4aea2afffbfd2d4bc0ce72be41047
Prem <premcs.v@gmail.com>
2026-02-03 13:27:33 +0530

    jitpack private configuerd

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -8,7 +8,13 @@ pluginManagement {
             }
         }
         mavenCentral()
-        maven { url = uri("https://jitpack.io") }
+        maven {
+            url = uri("https://jitpack.io")
+            credentials {
+                username = "jp_bj3d32o7qm68rsvspu9bi04dsr"
+                password = ""
+            }
+        }
         gradlePluginPortal()
     }
 }
@@ -17,7 +23,13 @@ dependencyResolutionManagement {
     repositories {
         google()
         mavenCentral()
-        maven { url = uri("https://jitpack.io") }
+        maven {
+            url = uri("https://jitpack.io")
+            credentials {
+                username = "jp_bj3d32o7qm68rsvspu9bi04dsr"
+                password = ""
+            }
+        }
     }
 }
 
