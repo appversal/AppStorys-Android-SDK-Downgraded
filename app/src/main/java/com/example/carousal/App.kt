75caea8b7c7b40125de39f4c1ea71aee2a60caed
Prem <premcs.v@gmail.com>
2026-05-21 16:32:48 +0530

    prod fix

--- app/src/main/java/com/example/carousal/App.kt
+++ app/src/main/java/com/example/carousal/App.kt
@@ -25,12 +25,12 @@ class App : Application() {
             context = this,
 //            appId =  "5a2ecc86-346a-4895-b14c-f662cc12071b",
 //            accountId = "70ddda52-2106-453b-919a-d63d1287bdf7",
-//            appId = "9e1b21a2-350a-4592-918c-2a19a73f249a",  // prod test
-//            accountId = "4350bf8e-0c9a-46bd-b953-abb65ab21d11",  // prod test
-            appId = "f69bdccf-b20f-4938-b39e-7075d76db791",  // dev test
-            accountId = "12a9eac5-94ee-4735-9aa6-b8a94cb8fbbb",  // dev test
+            appId = "9e1b21a2-350a-4592-918c-2a19a73f249a",  // prod test
+            accountId = "4350bf8e-0c9a-46bd-b953-abb65ab21d11",  // prod test
+//            appId = "f69bdccf-b20f-4938-b39e-7075d76db791",  // dev test
+//            accountId = "12a9eac5-94ee-4735-9aa6-b8a94cb8fbbb",  // dev test
 //            userId = userId,
-            userId = "nameispremagain",
+            userId = "nameisprem",
 //            userId = "b119f894-37fe-44a2-9108-77d06dedf912",
             navigateToScreen = { screen ->
                 println("Navigating to $screen")
